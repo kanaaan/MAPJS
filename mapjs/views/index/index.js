e3cf8bea19f6a73303cd5d2602e0971e5d576657
Kanaaan <mapjs@mapjs.com>
2016-01-31 20:50:32 +0100

    * Add HighCharts example and add jquery dependencies

--- mapjs/views/index/index.js
+++ mapjs/views/index/index.js
@@ -46,7 +46,47 @@
         }),
 
         GraphtestClicked: WinJS.UI.eventHandler(function (ev) {
-            window.location.href = "http://www.highcharts.com/demo/3d-column-interactive";
+            //window.location.href = "http://www.highcharts.com/demo/3d-column-interactive";
+            $('#container').highcharts({
+                title: {
+                    text: 'Monthly Average Temperature',
+                    x: -20 //center
+                },
+                subtitle: {
+                    text: 'Source: WorldClimate.com',
+                    x: -20
+                },
+                xAxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Temperature (°C)'
+                    },
+                    plotLines: [{
+                            value: 0,
+                            width: 1,
+                            color: '#808080'
+                        }]
+                },
+                tooltip: {
+                    valueSuffix: '°C'
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle',
+                    borderWidth: 0
+                },
+                series: [{
+                        name: 'Tokyo',
+                        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                    }, {
+                        name: 'New York',
+                        data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+                    }]
+            });
         }),
     };
     
